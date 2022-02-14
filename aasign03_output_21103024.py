11b3cf3bc1ff38d6cdda60f10c78565a5aacf7af
gaurav09122002 <97427927+gaurav09122002@users.noreply.github.com>
2022-02-14 09:36:34 +0530

    Add files via upload

---
+++ aasign03_output_21103024.py
@@ -0,0 +1,87 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Gaurav\OneDrive\Desktop\21103024 python\assign03_input_21103024.py
+QUESTION 1
+ENTER ANY STRING: gaurav
+{'g': 1, 'a': 2, 'u': 1, 'r': 1, 'v': 1}
+
+
+
+QUESTION 2
+ENTER THE DAY: 29
+ENTER THE MONTH OF THE YEAR: 2
+ENTER THE YEAR: 1900
+INVALID DATE TRY AGAIN
+ENTER THE DAY: 29
+ENTER THE MONTH OF THE YEAR: 2
+ENTER THE YEAR: 2000
+1 / 3 / 2000
+
+
+
+QUESTION 3
+Enter elements of a list separated by space 3 9 10
+list:  3 9 10
+[(3, 9), (9, 81), (10, 100)]
+
+
+
+QUESTION 4
+Enter Grade Point: 8
+Your Grade is 'B+' and Very Good performance
+
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+
+QUESTION 6
+Enter SID: 21103024
+Enter Name: GAUARV
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103028
+Enter Name: ANKUR
+Do you want to enter another student entry(Y or N):21103008
+Invalid input!
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103008
+Enter Name: SHAUNAK
+Do you want to enter another student entry(Y or N):N
+a. {21103024: 'GAUARV', 21103028: 'ANKUR', 21103008: 'SHAUNAK'}
+b. {21103028: 'ANKUR', 21103024: 'GAUARV', 21103008: 'SHAUNAK'}
+c. {21103008: 'SHAUNAK', 21103024: 'GAUARV', 21103028: 'ANKUR'}
+Search Name with SID: 21103028
+d. ANKUR
+
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 9
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 6.0
+
+
+
+QUESTION 8
+a. {1, 3, 5, 6, 8}
+b. {3, 6, 8, 9, 13, 17}
+c. {1, 2, 4, 5}
+d. {6, 7, 8, 9, 10}
+e. {10, 7}
