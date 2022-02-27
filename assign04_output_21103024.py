6b174d10a070190b3ad266d50eb7adc35cb1bc51
gaurav09122002 <97427927+gaurav09122002@users.noreply.github.com>
2022-02-27 19:18:13 +0530

    Add files via upload

---
+++ assign04_output_21103024.py
@@ -0,0 +1,72 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Gaurav\OneDrive\Desktop\21103024 python\assign04_input_21103024.py
+QUESTION 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+QUESTION 2
+Enter the size of pascals triangle: 6
+USING FOR LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING WHILE LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+QUESTION 3
+Enter 2 numbers: 20 5
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+b. The quotient is non zero and remainder is zero.
+c. Filtered out numbers that are greater than 4 are : [8, 5, 9, 5, 10, 6]
+d. Set: {5, 6, 8, 9, 10}
+e. Immutable set: frozenset({5, 6, 8, 9, 10})
+f. Hash value of the max value from the above set: 10
+
+
+QUESTION 4 
+Object created
+The name of the student it GAURAV GOYAL and SID is 21103024.
+object destroyed
+
+
+QUESTION 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+QUESTION 6
+Enter the word: listen
+Enter a new meaningful word using the exact same letters to test your friendship: silent
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
